ad79bd34267ff55e125a1c384b8169cb0d92bb18
Roland Oruche <rolandoruche@mwc-065255.dhcp.missouri.edu>
2019-06-29 12:36:07 -0500

    Added facial expressions

--- protected/humhub/modules/session/widgets/Menu.php
+++ protected/humhub/modules/session/widgets/Menu.php
@@ -54,6 +54,16 @@ class Menu extends \humhub\widgets\BaseMenu
             'isActive' => (Yii::$app->controller->id == "eeg" && (Yii::$app->controller->action->id == "index" || Yii::$app->controller->action->id == 'home') && Yii::$app->controller->module->id == "session")
         ));
 
+        /* Add adding Facial Expressions info */
+        $this->addItem(array(
+            'label' => Yii::t('SessionModule.widgets_SessionMenuWidget', 'Facial Expressions'),
+            'group' => 'modules',
+            'url' => $this->session->createUrl('/session/expressions'),
+            'icon' => '<i class="fa fa-smile-o"></i>',
+            'sortOrder' => 100,
+            'isActive' => (Yii::$app->controller->id == "expressions" && (Yii::$app->controller->action->id == "index" || Yii::$app->controller->action->id == 'home') && Yii::$app->controller->module->id == "session")
+        ));
+
         parent::init();
     }
 
