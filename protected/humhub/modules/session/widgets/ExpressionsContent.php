82af48c1797698fc3f9d77614f84d9ba03a9e674
Roland Oruche <rolandoruche@mwc-071143.dhcp.missouri.edu>
2019-06-29 21:03:52 -0500

    Adding expressions content files

---
+++ protected/humhub/modules/session/widgets/ExpressionsContent.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace humhub\modules\session\widgets;
+
+use Yii;
+use humhub\components\Widget;
+use yii\data\ActiveDataProvider;
+use humhub\modules\user\models\User;
+use humhub\modules\session\models\Tokens;
+use humhub\modules\session\models\SessionMembership;
+use humhub\modules\content\components\ContentContainerActiveRecord;
+
+class ExpressionsContent extends Widget {
+    /** 
+     * @var string
+     */
+    public $content = '';
+
+
+    /**
+     * @var ContentContainerActiveRecord
+     */
+    public $contentContatiner;
+
+    public function run() 
+    {
+        echo "Hello, World!(1)";
+    }
+}
