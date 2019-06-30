936062b47ca1786f909e2dfc4dc879b7e1b9ed47
Roland Oruche <rolandoruche@mwc-065057.dhcp.missouri.edu>
2019-06-30 11:57:07 -0500

    Adding expressions page

--- protected/humhub/modules/session/controllers/ExpressionsController.php
+++ protected/humhub/modules/session/controllers/ExpressionsController.php
@@ -9,10 +9,28 @@ use humhub\modules\stream\actions\ContentContainerStream;
 
 class ExpressionsController extends \humhub\modules\content\components\ContentContainerController
 {
+     /**
+     * @inheritdoc
+     */
+    public $adminOnly = false;
+
     /**
      * @inheritdoc
      */
-    public function init() {
-        return;
+    public function init()
+    {
+        $this->appendPageTitle(Yii::t('AdminModule.base', 'Facial Expressions'));
+        return parent::init();
     }
+
+    public function actionIndex()
+    {
+        // $session = $this->contentContainer;
+        $this->subLayout = '@humhub/modules/session/views/expressions/index';
+
+        $members = \humhub\modules\session\models\SessionMembership::getSessionMembersQuery($this->session)->all();
+        return $this->render('index');
+    }
+
+
 }
