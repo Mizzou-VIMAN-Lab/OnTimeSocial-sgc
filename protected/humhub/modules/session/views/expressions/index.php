f6dd47e6a031b7628ac6ea6d86359a078d98175e
Roland Oruche <rolandoruche@mwc-065057.dhcp.missouri.edu>
2019-07-01 22:21:22 -0500

    bug fixes...

--- protected/humhub/modules/session/views/expressions/index.php
+++ protected/humhub/modules/session/views/expressions/index.php
@@ -6,11 +6,11 @@
 use yii\helpers\Html;
 $session = $this->context->contentContainer;
 ?>
-
 <div class="container session-layout-container">
     <div class="row">
         <div class="col-md-12">
             <?php echo humhub\modules\session\widgets\Header::widget(['session' => $session]); ?>
+
         </div>
     </div>
     <div class="row">
@@ -18,14 +18,6 @@ $session = $this->context->contentContainer;
             <?php echo \humhub\modules\session\widgets\Menu::widget(['session' => $session]); ?>
             <br>
         </div>
-        <div class="container">
-            <div class="col-md-10 panel layout-content-container">
-                <p>Hello, World! Hope This works!!</p>
-                <?php /* echo \humhub\modules\session\widgets\ExpressionsContent::widget(['session' => $session]); */?>
-                <!-- 
-                    Get content from ExpressionsContent class
-                -->
-            </div>
-        </div>
+            <?= $content; ?>
     </div>
 </div>
