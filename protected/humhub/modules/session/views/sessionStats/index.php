e341c1d2dcdc7c1f45a08baf6ee91be457e35abf
Roland Oruche <rolandoruche@Rolands-MacBook-Pro.local>
2019-08-05 12:40:40 -0500

    Added session stats tab

---
+++ protected/humhub/modules/session/views/sessionStats/index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Developed my Roland Oruche, University of Missouri
+ */
+
+$session = $this->context->contentContainer;
+?>
+<div class="container session-layout-container">
+    <div class="row">
+        <div class="col-md-12">
+            <?php echo humhub\modules\session\widgets\Header::widget(['session' => $session]); ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-2 layout-nav-container">
+            <?php echo \humhub\modules\session\widgets\Menu::widget(['session' => $session]); ?>
+            <br>
+        </div>
+            <?= \humhub\modules\session\widgets\SessionStatsContent::widget([
+                    'contentContainer' => $session
+            ]) ?>
+    </div>
+</div>
--- protected/humhub/modules/session/widgets/Menu.php
+++ protected/humhub/modules/session/widgets/Menu.php
@@ -42,7 +42,17 @@ class Menu extends \humhub\widgets\BaseMenu
             'url' => $this->session->createUrl('/session/info'),
             'icon' => '<i class="fa fa-bars"></i>',
             'sortOrder' => 100,
-            'isActive' => (Yii::$app->controller->id == "info" && (Yii::$app->controller->action->id == "index" || Yii::$app->controller->action->id == 'home') && Yii::$app->controller->module->id == "session"),
+            'isActive' => (Yii::$app->controller->id == "info" && (Yii::$app->controller->action->id == "index" || Yii::$app->controller->action->id == 'home') && Yii::$app->controller->module->id == "session")
+        ));
+
+        /* Add adding Session Statistics info */
+        $this->addItem(array(
+            'label' => Yii::t('SessionModule.widgets_SessionMenuWidget', 'Session Stats'),
+            'group' => 'modules',
+            'url' => $this->session->createUrl('/session/sessionStats'),
+            'icon' => '<i class="fas fa-chart-pie"></i>',
+            'sortOrder' => 100,
+            'isActive' => (Yii::$app->controller->id == "sessionStats" && (Yii::$app->controller->action->id == "index" || Yii::$app->controller->action->id == 'home') && Yii::$app->controller->module->id == "session")
         ));
 
         $this->addItem(array(
