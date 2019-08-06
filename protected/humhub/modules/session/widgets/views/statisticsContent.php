8da0a6316fbbce94bb8ad26e2fc8a2722f7380c1
Roland Oruche <rolandoruche@Rolands-MacBook-Pro.local>
2019-08-05 20:00:01 -0500

    Fixing errors

--- protected/humhub/modules/session/widgets/views/statisticsContent.php
+++ protected/humhub/modules/session/widgets/views/statisticsContent.php
@@ -15,23 +15,21 @@ use yii\widgets\ActiveForm;
 use humhub\modules\space\models\Space;
 use humhub\modules\session\widgets\StatisticsContent;
 use humhub\widgets\Tabs;
-use humhub\modules\admin\widgets\SpaceGridView;
 ?>
 
 <?php
-    echo SpaceGridView::widget([
-        'dataProvider' => $dataProvider,
-        'columns' => [
+    echo Tabs::widget([
+        'items' => [
             [
-                'caption' => 'Main',
-                //'content' => 'This is some content...',
-                'options' => ['style' => 'min-width:200px;']
-                //'active' => true
+                'label' => 'Main',
+                'content' => 'This is some content...',
+                'options' => ['style' => 'min-width:200px;'],
+                'active' => true
             ],
             [
-                'caption' => 'Details',
-                //'content' => 'This is also some content...',
-                'options' => ['style' => 'min-width:200px;']
+                'label' => 'Details',
+                'content' => 'This is also some content...',
+                'options' => ['style' => 'min-width:200px;'],
             ],
         ],
     ]);
